76945c42ae25a6d47bd7405b2670bbbf521ea196
Michael J. Ryan <tracker1@gmail.com>
2015-02-11 15:34:49 -0700

    include window.Promise shim

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ module.exports = getBrowserFetch() || getNodeFetch();
 
 function getBrowserFetch() {
   if (!this.window) return;
-  if (this.window.fetch) return this.window.fetch; //use existing implementation
-  return require('./browser-fetch');
+  if (this.window.fetch) return this.window.fetch; //use existing implementation, if it exists
+  if (!this.window.Promise) this.window.Promise = require('es6-promise').Promise; //shim window.Promise if needed
+  return thiw.window.fetch = require('./browser-fetch'); //shim window.fetch and return module
 }
 
 function getNodeFetch(){
   //hack to prevent node-fetch implementation from being browserified
   return (function(n){ 
-    return require(n); 
+    return require(n);
   }('./node-fetch'));
 }
