node-fetch/node-fetch

commit 1592ca1148e657110fd6f64dc8b29a576cd1da1c

Use caseless Headers handling
This is unfortunately impossible to test, since the Node.js HTTP library
lower-cases all incoming headers. However, this matters for outgoing
HTTP requests. See the linked issues from the linked Fetch Standard pull
request.

See: https://github.com/whatwg/fetch/pull/476

src/headers.js
@@ -8,20 +8,36 @@
 const invalidTokenRegex = /[^\^_`a-zA-Z\-0-9!#$%&'*+.|~]/;
 const invalidHeaderCharRegex = /[^\t\x20-\x7e\x80-\xff]/;
 
-function sanitizeName(name) {
-	name += '';
+function validateName(name) {
+	name = `${name}`;
 	if (invalidTokenRegex.test(name)) {
 		throw new TypeError(`${name} is not a legal HTTP header name`);
 	}
-	return name.toLowerCase();
 }
 
-function sanitizeValue(value) {
-	value += '';
+function validateValue(value) {
+	value = `${value}`;
 	if (invalidHeaderCharRegex.test(value)) {
 		throw new TypeError(`${value} is not a legal HTTP header value`);
 	}
-	return value;
+}
+
+/**
+ * Find the key in the map object given a header name.
+ *
+ * Returns undefined if not found.
+ *
+ * @param   String  name  Header name
+ * @return  String|Undefined
+ */
+function find(map, name) {
+	name = name.toLowerCase();
+	for (const key in map) {
+		if (key.toLowerCase() === name) {
+			return key;
+		}
+	}
+	return undefined;
 }
 
 const MAP = Symbol('map');
@@ -88,18 +104,20 @@ export default class Headers {
 	}
 
 	/**
-	 * Return first header value given name
+	 * Return combined header value given name
 	 *
 	 * @param   String  name  Header name
 	 * @return  Mixed
 	 */
 	get(name) {
-		const list = this[MAP][sanitizeName(name)];
-		if (!list) {
+		name = `${name}`;
+		validateName(name);
+		const key = find(this[MAP], name);
+		if (key === undefined) {
 			return null;
 		}
 
-		return list.join(', ');
+		return this[MAP][key].join(', ');
 	}
 
 	/**
@@ -110,12 +128,12 @@ export default class Headers {
 	 * @return  Void
 	 */
 	forEach(callback, thisArg = undefined) {
-		let pairs = getHeaderPairs(this);
+		let pairs = getHeaders(this);
 		let i = 0;
 		while (i < pairs.length) {
 			const [name, value] = pairs[i];
 			callback.call(thisArg, value, name, this);
-			pairs = getHeaderPairs(this);
+			pairs = getHeaders(this);
 			i++;
 		}
 	}
@@ -128,7 +146,12 @@ export default class Headers {
 	 * @return  Void
 	 */
 	set(name, value) {
-		this[MAP][sanitizeName(name)] = [sanitizeValue(value)];
+		name = `${name}`;
+		value = `${value}`;
+		validateName(name);
+		validateValue(value);
+		const key = find(this[MAP], name);
+		this[MAP][key !== undefined ? key : name] = [value];
 	}
 
 	/**
@@ -139,12 +162,16 @@ export default class Headers {
 	 * @return  Void
 	 */
 	append(name, value) {
-		if (!this.has(name)) {
-			this.set(name, value);
-			return;
+		name = `${name}`;
+		value = `${value}`;
+		validateName(name);
+		validateValue(value);
+		const key = find(this[MAP], name);
+		if (key !== undefined) {
+			this[MAP][key].push(value);
+		} else {
+			this[MAP][name] = [value];
 		}
-
-		this[MAP][sanitizeName(name)].push(sanitizeValue(value));
 	}
 
 	/**
@@ -154,7 +181,9 @@ export default class Headers {
 	 * @return  Boolean
 	 */
 	has(name) {
-		return !!this[MAP][sanitizeName(name)];
+		name = `${name}`;
+		validateName(name);
+		return find(this[MAP], name) !== undefined;
 	}
 
 	/**
@@ -164,7 +193,12 @@ export default class Headers {
 	 * @return  Void
 	 */
 	delete(name) {
-		delete this[MAP][sanitizeName(name)];
+		name = `${name}`;
+		validateName(name);
+		const key = find(this[MAP], name);
+		if (key !== undefined) {
+			delete this[MAP][key];
+		}
 	};
 
 	/**
@@ -226,12 +260,14 @@ Object.defineProperties(Headers.prototype, {
 	entries: { enumerable: true }
 });
 
-function getHeaderPairs(headers, kind) {
+function getHeaders(headers, kind = 'key+value') {
 	const keys = Object.keys(headers[MAP]).sort();
 	return keys.map(
 		kind === 'key' ?
-			k => [k] :
-			k => [k, headers.get(k)]
+			k => k.toLowerCase() :
+			kind === 'value' ?
+				k => headers[MAP][k].join(', ') :
+				k => [k.toLowerCase(), headers[MAP][k].join(', ')]
 	);
 }
 
@@ -260,7 +296,7 @@ const HeadersIteratorPrototype = Object.setPrototypeOf({
 			kind,
 			index
 		} = this[INTERNAL];
-		const values = getHeaderPairs(target, kind);
+		const values = getHeaders(target, kind);
 		const len = values.length;
 		if (index >= len) {
 			return {
@@ -269,20 +305,10 @@ const HeadersIteratorPrototype = Object.setPrototypeOf({
 			};
 		}
 
-		const pair = values[index];
 		this[INTERNAL].index = index + 1;
 
-		let result;
-		if (kind === 'key') {
-			result = pair[0];
-		} else if (kind === 'value') {
-			result = pair[1];
-		} else {
-			result = pair;
-		}
-
 		return {
-			value: result,
+			value: values[index],
 			done: false
 		};
 	}

commit f175187e3edd3b63e846862976e93e5ac3689cc6

Fix custom Host header with arbitrary case
Regression since 1592ca1148e657110fd6f64dc8b29a576cd1da1c.

Fixes: #416
Fixes: #425

src/headers.js
@@ -324,6 +324,25 @@ Object.defineProperty(HeadersIteratorPrototype, Symbol.toStringTag, {
 });
 
 /**
+ * Export the Headers object in a form that Node.js can consume.
+ *
+ * @param   Headers  headers
+ * @return  Object
+ */
+export function exportNodeCompatibleHeaders(headers) {
+	const obj = Object.assign({ __proto__: null }, headers[MAP]);
+
+	// http.request() only supports string as Host header. This hack makes
+	// specifying custom Host header possible.
+	const hostHeaderKey = find(headers[MAP], 'Host');
+	if (hostHeaderKey !== undefined) {
+		obj[hostHeaderKey] = obj[hostHeaderKey][0];
+	}
+
+	return obj;
+}
+
+/**
  * Create a Headers object from an object of headers, ignoring those that do
  * not conform to HTTP grammar productions.
  *

src/index.js
@@ -43,11 +43,6 @@ export default function fetch(url, opts) {
 
 		const send = (options.protocol === 'https:' ? https : http).request;
 
-		// http.request only support string as host header, this hack make custom host header possible
-		if (options.headers.host) {
-			options.headers.host = options.headers.host[0];
-		}
-
 		// send request
 		const req = send(options);
 		let reqTimeout;

src/request.js
@@ -7,7 +7,7 @@
  * All spec algorithm step numbers are based on https://fetch.spec.whatwg.org/commit-snapshots/ae716822cb3a61843226cd090eefc6589446c1d2/.
  */
 
-import Headers from './headers.js';
+import Headers, { exportNodeCompatibleHeaders } from './headers.js';
 import Body, { clone, extractContentType, getTotalBytes } from './body';
 
 const { format: format_url, parse: parse_url } = require('url');
@@ -200,7 +200,7 @@ export function getNodeRequestOptions(request) {
 
 	return Object.assign({}, parsedURL, {
 		method: request.method,
-		headers: headers.raw(),
+		headers: exportNodeCompatibleHeaders(headers),
 		agent: request.agent
 	});
 }

test/test.js
@@ -208,6 +208,20 @@ describe('node-fetch', () => {
 		});
 	});
 
+	it('should accept custom HoSt header', function() {
+		const url = `${base}inspect`;
+		const opts = {
+			headers: {
+				HoSt: 'example.com'
+			}
+		};
+		return fetch(url, opts).then(res => {
+			return res.json();
+		}).then(res => {
+			expect(res.headers['host']).to.equal('example.com');
+		});
+	});
+
 	it('should follow redirect code 301', function() {
 		const url = `${base}redirect/301`;
 		return fetch(url).then(res => {