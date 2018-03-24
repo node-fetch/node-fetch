03bb435b32017101ed4e7bea725e582e6a8b008c
Mark Herhold <mark.herhold@gmail.com>
2018-03-24 13:46:06 -0400

    Support TypeScript import in browser

--- browser.js
+++ browser.js
@@ -1,4 +1,8 @@
 module.exports = exports = window.fetch;
+
+// Needed for TypeScript.
+exports.default = window.fetch;
+
 exports.Headers = window.Headers;
 exports.Request = window.Request;
 exports.Response = window.Response;
