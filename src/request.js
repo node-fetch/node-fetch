7019994ade0bc9b5ac061673121de72ab8059e02
Daniel K <FredyC@users.noreply.github.com>
2018-05-26 15:22:30 +0200

    Fix HTTPS for Node 8 & 9

--- src/request.js
+++ src/request.js
@@ -98,6 +98,7 @@ export default class Request {
 			input.compress : true;
 		this.counter = init.counter || input.counter || 0;
 		this.agent = init.agent || input.agent;
+		this.ecdhCurve = 'auto';
 	}
 
 	get method() {
