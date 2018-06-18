6f3d0552cbf478312fbf55f62ccdf3b260bec4b3
Bernhard K. Weisshuhn <bkw@codingforce.com>
2018-06-18 22:14:03 +0200

    explicitly specify defaults for lookup (review)

--- src/request.js
+++ src/request.js
@@ -11,6 +11,7 @@ import Headers, { exportNodeCompatibleHeaders } from './headers.js';
 import Body, { clone, extractContentType, getTotalBytes } from './body';
 
 const { format: format_url, parse: parse_url } = require('url');
+const dns = require('dns');
 
 const INTERNALS = Symbol('Request internals');
 
@@ -98,7 +99,7 @@ export default class Request {
 			input.compress : true;
 		this.counter = init.counter || input.counter || 0;
 		this.agent = init.agent || input.agent;
-		this.lookup = init.lookup || input.lookup;
+		this.lookup = init.lookup || input.lookup || dns.lookup;
 	}
 
 	get method() {
