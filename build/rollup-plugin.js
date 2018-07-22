6868e4aa59a1bcc9ef263c9c2cfa8beca1e6779e
Timothy Gu <timothygu99@gmail.com>
2018-07-22 09:51:06 -0700

    Add __esModule property to exports object
    Fixes: #442

--- build/rollup-plugin.js
+++ build/rollup-plugin.js
@@ -7,6 +7,7 @@ export default function tweakDefault() {
         var matches = /^(exports(?:\['default']|\.default)) = (.*);$/.exec(line);
         if (matches) {
           lines[i] = 'module.exports = exports = ' + matches[2] + ';\n' +
+                     'Object.defineProperty(exports, "__esModule", { value: true });\n' +
                      matches[1] + ' = exports;';
           break;
         }
