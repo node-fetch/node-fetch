1b4d7bf4bd08cdeb68245368512e51850e126f1a
Bruno Lemos <brunohplemos@gmail.com>
2018-10-18 01:53:02 -0300

    Polyfill self

--- browser.js
+++ browser.js
@@ -1,5 +1,15 @@
 "use strict";
 
+function getSelf() {
+  if (typeof self !== 'undefined') return self;
+
+  if (typeof this !== 'undefined') return this;
+  if (typeof global !== 'undefined') return global;
+  if (typeof window !== 'undefined') return window;
+  throw new Error('Failed to access global variable "self".');
+}
+
+;(function(self) {
 module.exports = exports = self.fetch;
 
 // Needed for TypeScript and Webpack.
@@ -8,3 +18,4 @@ exports.default = self.fetch.bind(self);
 exports.Headers = self.Headers;
 exports.Request = self.Request;
 exports.Response = self.Response;
+})(getSelf());
