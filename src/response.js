f79bffc56c647abe0262956889a2cff65524e51c
Jimmy Wärting <jimmy@warting.se>
2018-11-08 11:39:29 +0100

    Clone urlsearchparams to avoid mutation Set correct content-type in response constructor

--- src/response.js
+++ src/response.js
@@ -7,7 +7,7 @@
 
 import { STATUS_CODES } from 'http';
 import Headers from './headers.js';
-import Body, { clone } from './body';
+import Body, { clone, extractContentType } from './body';
 
 const INTERNALS = Symbol('Response internals');
 
@@ -23,12 +23,20 @@ export default class Response {
 		Body.call(this, body, opts);
 
 		const status = opts.status || 200;
+    const headers = new Headers(opts.headers)
+
+		if (body != null && !headers.has('Content-Type')) {
+			const contentType = extractContentType(body);
+			if (contentType) {
+				headers.append('Content-Type', contentType);
+			}
+		}
 
 		this[INTERNALS] = {
 			url: opts.url,
 			status,
 			statusText: opts.statusText || STATUS_CODES[status],
-			headers: new Headers(opts.headers)
+			headers
 		};
 	}
 
