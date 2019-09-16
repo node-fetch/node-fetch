91296a9492399e04d685aae41fc1ed10c3f212cf
Boris Bosiljcic <b.bosiljcic@gmail.com>
2019-09-16 11:32:40 +0200

    check for global.fetch before binding it

--- browser.js
+++ browser.js
@@ -16,7 +16,9 @@ var global = getGlobal();
 module.exports = exports = global.fetch;
 
 // Needed for TypeScript and Webpack.
-exports.default = global.fetch.bind(global);
+if (global.fetch) {
+	exports.default = global.fetch.bind(global);
+}
 
 exports.Headers = global.Headers;
 exports.Request = global.Request;
