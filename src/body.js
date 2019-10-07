6a5d192034a0f438551dffb6d2d8df2c00921d16
dsuket <dai.takaoka@gmail.com>
2019-10-07 15:58:27 +0900

    fix: Properly parse meta tag when parameters are reversed (#682)

--- src/body.js
+++ src/body.js
@@ -306,6 +306,12 @@ function convertBody(buffer, headers) {
 	// html4
 	if (!res && str) {
 		res = /<meta[\s]+?http-equiv=(['"])content-type\1[\s]+?content=(['"])(.+?)\2/i.exec(str);
+		if (!res) {
+			res = /<meta[\s]+?content=(['"])(.+?)\1[\s]+?http-equiv=(['"])content-type\3/i.exec(str);
+			if (res) {
+				res.pop(); // drop last quote
+			}
+		}
 
 		if (res) {
 			res = /charset=(.*)/i.exec(res.pop());
