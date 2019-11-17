86b32d0a6ae1964978616a52f1a17bd053888ad0
Richie Bendall <richiebendall@gmail.com>
2019-11-17 18:07:07 +1300

    chore: Remove logging statement
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- src/request.js
+++ src/request.js
@@ -7,16 +7,16 @@
  * All spec algorithm step numbers are based on https://fetch.spec.whatwg.org/commit-snapshots/ae716822cb3a61843226cd090eefc6589446c1d2/.
  */
 
-import {parse as parseUrl, format as formatUrl} from 'url';
-import Stream from 'stream';
-import utf8 from 'utf8';
-import Headers, {exportNodeCompatibleHeaders} from './headers';
-import Body, {clone, extractContentType, getTotalBytes} from './body';
-import {isAbortSignal} from './utils/is';
+import { parse as parseUrl, format as formatUrl } from "url";
+import Stream from "stream";
+import utf8 from "utf8";
+import Headers, { exportNodeCompatibleHeaders } from "./headers";
+import Body, { clone, extractContentType, getTotalBytes } from "./body";
+import { isAbortSignal } from "./utils/is";
 
-const INTERNALS = Symbol('Request internals');
+const INTERNALS = Symbol("Request internals")
 
-const streamDestructionSupported = 'destroy' in Stream.Readable.prototype;
+const streamDestructionSupported = "destroy" in Stream.Readable.prototype
 
 /**
  * Check if `obj` is an instance of Request.
@@ -25,10 +25,10 @@ const streamDestructionSupported = 'destroy' in Stream.Readable.prototype;
  * @return {boolean}
  */
 function isRequest(obj) {
-	return (
-		typeof obj === 'object' &&
-		typeof obj[INTERNALS] === 'object'
-	);
+    return (
+        typeof obj === "object" &&
+		typeof obj[INTERNALS] === "object"
+    )
 }
 
 /**
@@ -39,132 +39,132 @@ function isRequest(obj) {
  * @return  Void
  */
 export default class Request {
-	constructor(input, init = {}) {
-		let parsedURL;
+    constructor(input, init = {}) {
+        let parsedURL
 
 		// Normalize input and force URL to be encoded as UTF-8 (https://github.com/bitinn/node-fetch/issues/245)
 		if (!isRequest(input)) {
-			if (input && input.href) {
-				// In order to support Node.js' Url objects; though WHATWG's URL objects
-				// will fall into this branch also (since their `toString()` will return
-				// `href` property anyway)
-				parsedURL = parseUrl(utf8.encode(input.href));
+            if (input && input.href) {
+                // In order to support Node.js' Url objects; though WHATWG's URL objects
+                // will fall into this branch also (since their `toString()` will return
+                // `href` property anyway)
+                parsedURL = parseUrl(utf8.encode(input.href))
 			} else {
-				// Coerce input to a string before attempting to parse
-				parsedURL = parseUrl(utf8.encode(`${input}`));
+                // Coerce input to a string before attempting to parse
+                parsedURL = parseUrl(utf8.encode(`${input}`))
 			}
 
-			input = {};
+            input = {}
 		} else {
-			parsedURL = parseUrl(utf8.encode(input.url));
+            parsedURL = parseUrl(utf8.encode(input.url))
 		}
 
-		let method = init.method || input.method || 'GET';
-		method = method.toUpperCase();
+        let method = init.method || input.method || "GET";
+        method = method.toUpperCase()
 
 		if ((init.body != null || isRequest(input) && input.body !== null) &&
-			(method === 'GET' || method === 'HEAD')) {
-			throw new TypeError('Request with GET/HEAD method cannot have body');
+			(method === "GET" || method === "HEAD")) {
+            throw new TypeError("Request with GET/HEAD method cannot have body")
 		}
 
-		const inputBody = init.body != null ?
-			init.body :
-			(isRequest(input) && input.body !== null ?
-				clone(input) :
-				null);
+        const inputBody = init.body != null ?
+            init.body :
+            (isRequest(input) && input.body !== null ?
+                clone(input) :
+                null)
 
 		Body.call(this, inputBody, {
-			timeout: init.timeout || input.timeout || 0,
-			size: init.size || input.size || 0
-		});
+            timeout: init.timeout || input.timeout || 0,
+            size: init.size || input.size || 0,
+        })
 
-		const headers = new Headers(init.headers || input.headers || {});
+		const headers = new Headers(init.headers || input.headers || {})
 
-		if (inputBody != null && !headers.has('Content-Type')) {
-			const contentType = extractContentType(inputBody);
+		if (inputBody != null && !headers.has("Content-Type")) {
+            const contentType = extractContentType(inputBody)
 			if (contentType) {
-				headers.append('Content-Type', contentType);
+                headers.append("Content-Type", contentType)
 			}
+        }
+
+        let signal = isRequest(input) ?
+            input.signal :
+            null
+		if ("signal" in init) {
+            signal = init.signal
 		}
 
-		let signal = isRequest(input) ?
-			input.signal :
-			null;
-		if ('signal' in init) {
-			signal = init.signal;
+        if (signal != null && !isAbortSignal(signal)) {
+            throw new TypeError("Expected signal to be an instanceof AbortSignal")
 		}
 
-		if (signal != null && !isAbortSignal(signal)) {
-			throw new TypeError('Expected signal to be an instanceof AbortSignal');
-		}
-
-		this[INTERNALS] = {
-			method,
-			redirect: init.redirect || input.redirect || 'follow',
-			headers,
-			parsedURL,
-			signal
-		};
+        this[INTERNALS] = {
+            method,
+            redirect: init.redirect || input.redirect || "follow",
+            headers,
+            parsedURL,
+            signal,
+        }
 
 		// Node-fetch-only options
 		this.follow = init.follow !== undefined ?
-			init.follow : (input.follow !== undefined ?
-				input.follow : 20);
+            init.follow : (input.follow !== undefined ?
+                input.follow : 20)
 		this.compress = init.compress !== undefined ?
-			init.compress : (input.compress !== undefined ?
-				input.compress : true);
-		this.counter = init.counter || input.counter || 0;
-		this.agent = init.agent || input.agent;
-		this.highWaterMark = init.highWaterMark || input.highWaterMark;
+            init.compress : (input.compress !== undefined ?
+                input.compress : true)
+		this.counter = init.counter || input.counter || 0
+		this.agent = init.agent || input.agent
+		this.highWaterMark = init.highWaterMark || input.highWaterMark
 	}
 
-	get method() {
-		return this[INTERNALS].method;
+    get method() {
+        return this[INTERNALS].method
 	}
 
-	get url() {
-		return formatUrl(this[INTERNALS].parsedURL);
+    get url() {
+        return formatUrl(this[INTERNALS].parsedURL)
 	}
 
-	get headers() {
-		return this[INTERNALS].headers;
+    get headers() {
+        return this[INTERNALS].headers
 	}
 
-	get redirect() {
-		return this[INTERNALS].redirect;
+    get redirect() {
+        return this[INTERNALS].redirect
 	}
 
-	get signal() {
-		return this[INTERNALS].signal;
+    get signal() {
+        return this[INTERNALS].signal
 	}
 
-	/**
+    /**
 	 * Clone this request
 	 *
 	 * @return  Request
 	 */
-	clone() {
-		return new Request(this);
+    clone() {
+        return new Request(this)
 	}
 }
 
-Body.mixIn(Request.prototype);
+Body.mixIn(Request.prototype)
 
 Object.defineProperty(Request.prototype, Symbol.toStringTag, {
-	value: 'Request',
-	writable: false,
-	enumerable: false,
-	configurable: true
-});
+    value: "Request",
+    writable: false,
+    enumerable: false,
+    configurable: true,
+})
 
 Object.defineProperties(Request.prototype, {
-	method: {enumerable: true},
-	url: {enumerable: true},
-	headers: {enumerable: true},
-	redirect: {enumerable: true},
-	clone: {enumerable: true},
-	signal: {enumerable: true}
-});
+    method: { enumerable: true },
+    url: { enumerable: true },
+    headers: { enumerable: true },
+    redirect: { enumerable: true },
+    clone: { enumerable: true },
+    signal: { enumerable: true },
+})
 
 /**
  * Convert a Request to Node.js http request options.
@@ -173,76 +173,74 @@ Object.defineProperties(Request.prototype, {
  * @return  Object   The options object to be passed to http.request
  */
 export function getNodeRequestOptions(request) {
-	const {parsedURL} = request[INTERNALS];
-	const headers = new Headers(request[INTERNALS].headers);
+    const { parsedURL } = request[INTERNALS]
+	const headers = new Headers(request[INTERNALS].headers)
 
 	// Fetch step 1.3
-	if (!headers.has('Accept')) {
-		headers.set('Accept', '*/*');
+	if (!headers.has("Accept")) {
+        headers.set("Accept", "*/*")
 	}
 
-	// Console.log(parsedURL.protocol, parsedURL.hostname)
-
-	// Basic fetch
-	if (!parsedURL.protocol || !parsedURL.hostname) {
-		throw new TypeError('Only absolute URLs are supported');
+    // Basic fetch
+    if (!parsedURL.protocol || !parsedURL.hostname) {
+        throw new TypeError("Only absolute URLs are supported")
 	}
 
-	if (!/^https?:$/.test(parsedURL.protocol)) {
-		throw new TypeError('Only HTTP(S) protocols are supported');
+    if (!/^https?:$/.test(parsedURL.protocol)) {
+        throw new TypeError("Only HTTP(S) protocols are supported")
 	}
 
-	if (
-		request.signal &&
+    if (
+        request.signal &&
 		request.body instanceof Stream.Readable &&
 		!streamDestructionSupported
-	) {
-		throw new Error('Cancellation of streamed requests with AbortSignal is not supported');
+    ) {
+        throw new Error("Cancellation of streamed requests with AbortSignal is not supported")
 	}
 
-	// HTTP-network-or-cache fetch steps 2.4-2.7
-	let contentLengthValue = null;
+    // HTTP-network-or-cache fetch steps 2.4-2.7
+    let contentLengthValue = null
 	if (request.body == null && /^(POST|PUT)$/i.test(request.method)) {
-		contentLengthValue = '0';
-	}
+        contentLengthValue = "0";
+    }
 
-	if (request.body != null) {
-		const totalBytes = getTotalBytes(request);
-		if (typeof totalBytes === 'number') {
-			contentLengthValue = String(totalBytes);
+    if (request.body != null) {
+        const totalBytes = getTotalBytes(request)
+		if (typeof totalBytes === "number") {
+            contentLengthValue = String(totalBytes)
 		}
+    }
+
+    if (contentLengthValue) {
+        headers.set("Content-Length", contentLengthValue)
 	}
 
-	if (contentLengthValue) {
-		headers.set('Content-Length', contentLengthValue);
+    // HTTP-network-or-cache fetch step 2.11
+    if (!headers.has("User-Agent")) {
+        headers.set("User-Agent", "node-fetch/1.0 (+https://github.com/bitinn/node-fetch)")
 	}
 
-	// HTTP-network-or-cache fetch step 2.11
-	if (!headers.has('User-Agent')) {
-		headers.set('User-Agent', 'node-fetch/1.0 (+https://github.com/bitinn/node-fetch)');
+    // HTTP-network-or-cache fetch step 2.15
+    if (request.compress && !headers.has("Accept-Encoding")) {
+        headers.set("Accept-Encoding", "gzip,deflate")
 	}
 
-	// HTTP-network-or-cache fetch step 2.15
-	if (request.compress && !headers.has('Accept-Encoding')) {
-		headers.set('Accept-Encoding', 'gzip,deflate');
+    let { agent } = request
+	if (typeof agent === "function") {
+        agent = agent(parsedURL)
 	}
 
-	let {agent} = request;
-	if (typeof agent === 'function') {
-		agent = agent(parsedURL);
+    if (!headers.has("Connection") && !agent) {
+        headers.set("Connection", "close")
 	}
 
-	if (!headers.has('Connection') && !agent) {
-		headers.set('Connection', 'close');
-	}
+    // HTTP-network fetch step 4.2
+    // chunked encoding is handled by Node.js
 
-	// HTTP-network fetch step 4.2
-	// chunked encoding is handled by Node.js
-
-	return {
-		...parsedURL,
-		method: request.method,
-		headers: exportNodeCompatibleHeaders(headers),
-		agent
-	};
+    return {
+        ...parsedURL,
+        method: request.method,
+        headers: exportNodeCompatibleHeaders(headers),
+        agent,
+    }
 }
