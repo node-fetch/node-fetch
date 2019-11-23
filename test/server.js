af7008787c8f452e938ba2f8d6ed516a986bf169
Richie Bendall <richiebendall@gmail.com>
2019-11-24 10:32:11 +1300

    refactor: Replace `encoding` with `fetch-charset-detection`. (#694)
    * refactor: Replace `encoding` with `fetch-charset-detection`.
    
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>
    
    * refactor: Move writing to stream back to body.js
    
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>
    
    * refactor: Only put convertBody in fetch-charset-detection and refactor others.
    
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>
    
    * test: Readd tests for getTotalBytes and extractContentType
    
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>
    
    * chore: Revert package.json indention
    
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>
    
    * chore: Remove optional dependency
    
    * docs: Replace code for fetch-charset-detection with documentation.
    
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>
    
    * chore: Remove iconv-lite
    
    * fix: Use default export instead of named export for convertBody
    
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>
    
    * chore: Remove unneeded installation of fetch-charset-detection in the build

--- test/server.js
+++ test/server.js
@@ -3,11 +3,6 @@ import {parse} from 'url';
 import * as zlib from 'zlib';
 import {multipart as Multipart} from 'parted';
 
-let convert;
-try {
-	convert = require('encoding').convert; // eslint-disable-line import/no-unresolved
-} catch (_) { }
-
 export default class TestServer {
 	constructor() {
 		this.server = http.createServer(this.router);
@@ -214,63 +209,6 @@ export default class TestServer {
 			res.statusCode = 200;
 			res.setHeader('Content-Type', 'text/plain');
 			res.end('testtest');
-		}
-
-		if (p === '/encoding/gbk') {
-			res.statusCode = 200;
-			res.setHeader('Content-Type', 'text/html');
-			res.end(convert('<meta charset="gbk"><div>中文</div>', 'gbk'));
-		}
-
-		if (p === '/encoding/gb2312') {
-			res.statusCode = 200;
-			res.setHeader('Content-Type', 'text/html');
-			res.end(convert('<meta http-equiv="Content-Type" content="text/html; charset=gb2312"><div>中文</div>', 'gb2312'));
-		}
-
-		if (p === '/encoding/shift-jis') {
-			res.statusCode = 200;
-			res.setHeader('Content-Type', 'text/html; charset=Shift-JIS');
-			res.end(convert('<div>日本語</div>', 'Shift_JIS'));
-		}
-
-		if (p === '/encoding/euc-jp') {
-			res.statusCode = 200;
-			res.setHeader('Content-Type', 'text/xml');
-			res.end(convert('<?xml version="1.0" encoding="EUC-JP"?><title>日本語</title>', 'EUC-JP'));
-		}
-
-		if (p === '/encoding/utf8') {
-			res.statusCode = 200;
-			res.end('中文');
-		}
-
-		if (p === '/encoding/order1') {
-			res.statusCode = 200;
-			res.setHeader('Content-Type', 'charset=gbk; text/plain');
-			res.end(convert('中文', 'gbk'));
-		}
-
-		if (p === '/encoding/order2') {
-			res.statusCode = 200;
-			res.setHeader('Content-Type', 'text/plain; charset=gbk; qs=1');
-			res.end(convert('中文', 'gbk'));
-		}
-
-		if (p === '/encoding/chunked') {
-			res.statusCode = 200;
-			res.setHeader('Content-Type', 'text/html');
-			res.setHeader('Transfer-Encoding', 'chunked');
-			res.write('a'.repeat(10));
-			res.end(convert('<meta http-equiv="Content-Type" content="text/html; charset=Shift_JIS" /><div>日本語</div>', 'Shift_JIS'));
-		}
-
-		if (p === '/encoding/invalid') {
-			res.statusCode = 200;
-			res.setHeader('Content-Type', 'text/html');
-			res.setHeader('Transfer-Encoding', 'chunked');
-			res.write('a'.repeat(1200));
-			res.end(convert('中文', 'gbk'));
 		}
 
 		if (p === '/redirect/301') {
