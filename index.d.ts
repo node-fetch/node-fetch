9c55f371c13ed501c2ab6dc8519287ec44cd5a2c
Richie Bendall <richiebendall@gmail.com>
2020-02-06 17:21:40 +1300

    chore: Update typings comment
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,11 @@
-// Type definitions for node-fetch 2.5
-// Project: https://github.com/node-fetch/node-fetch
-// Definitions by: Torsten Werner <https://github.com/torstenwerner>
-//                 Niklas Lindgren <https://github.com/nikcorg>
-//                 Vinay Bedre <https://github.com/vinaybedre>
-//                 Antonio Román <https://github.com/kyranet>
-//                 Andrew Leedham <https://github.com/AndrewLeedham>
-//                 Jason Li <https://github.com/JasonLi914>
-//                 Brandon Wilson <https://github.com/wilsonianb>
-//                 Steve Faulkner <https://github.com/southpolesteve>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// Prior contributors: Torsten Werner <https://github.com/torstenwerner>
+//                     Niklas Lindgren <https://github.com/nikcorg>
+//                     Vinay Bedre <https://github.com/vinaybedre>
+//                     Antonio Román <https://github.com/kyranet>
+//                     Andrew Leedham <https://github.com/AndrewLeedham>
+//                     Jason Li <https://github.com/JasonLi914>
+//                     Brandon Wilson <https://github.com/wilsonianb>
+//                     Steve Faulkner <https://github.com/southpolesteve>
 
 /// <reference types="node" />
 
