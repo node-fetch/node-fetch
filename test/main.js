29ac7d116882566967aa786110bb3c49548db96e
Antoni Kepinski <xxczaki@pm.me>
2020-05-10 01:12:33 +0200

    revert

--- test/main.js
+++ test/main.js
@@ -2050,7 +2050,8 @@ describe('node-fetch', () => {
 		});
 	});
 
-	it('http2', () => {
+	it('http2', function () {
+		this.timeout(5000);
 		const url = 'https://http2.golang.org/';
 		return fetch(url).then(res => {
 			expect(res.status).to.equal(200);
