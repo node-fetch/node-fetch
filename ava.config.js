0e376f7f31eab86bd6beadff11e9c176cb40af51
Antoni Kepinski <xxczaki@pm.me>
2020-05-25 01:00:13 +0200

    replace esm with a flag (AVA)

--- ava.config.js
+++ ava.config.js
@@ -1,7 +1,7 @@
 export default {
 	files: ['test-new/**', '!test-new/utils'],
 	ignoredByWatcher: ['{coverage,docs,test-new,@types}/**'],
-	require: [
-		'esm'
+	nodeArguments: [
+		'--experimental-modules'
 	]
 };
