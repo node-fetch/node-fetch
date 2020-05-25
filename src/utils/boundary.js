56909afdbfd92d35decd94700920578b0ac6c851
octet-stream <nick.kruchinin@gmail.com>
2020-05-26 01:20:48 +0300

    Rewrite boundary helper.

--- src/utils/boundary.js
+++ src/utils/boundary.js
@@ -1,13 +1,6 @@
-// ? I don't know if it necessary here, but thios module might cause errors in CRA
-// ? See: https://github.com/ai/nanoid/issues/205
-// ? If anything happens, we could replace it with something else
-import {customAlphabet} from 'nanoid';
-
-const alpha = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
 /**
  * @api private
  */
-const boundary = customAlphabet(alpha, 22);
+const boundary = () => Math.random().toString(32).slice(2);
 
 export default boundary;
