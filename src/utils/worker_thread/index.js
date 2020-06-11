56b83907dad61818c852b5ff60eaced7dcee6c18
Konstantin Vyatkin <tino@vtkn.io>
2020-06-11 15:37:35 -0400

    lint

--- src/utils/worker_thread/index.js
+++ src/utils/worker_thread/index.js
@@ -2,4 +2,5 @@
 
 // Use --experimental-worker will need Nodejs v10.5.0 until 11.7
 const {builtinModules} = require('module');
+// eslint-disable-next-line node/no-unsupported-features/node-builtins
 module.exports = builtinModules.includes('worker_threads') ? require('worker_threads').Worker : false;
