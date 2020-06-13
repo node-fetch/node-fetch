de203646c269142b669ab1ca130d511b0f6a297e
Antoni Kepinski <a@kepinski.me>
2020-06-14 00:32:46 +0200

    add 2 ava config files

---
+++ ava.config.cjs
@@ -0,0 +1,4 @@
+module.exports = {
+	files: ['test-new/commonjs/test-artifact.js'],
+	ignoredByWatcher: ['{coverage,docs,test-new,@types}/**']
+};
--- ava.config.js
+++ ava.config.js
@@ -1,5 +1,5 @@
 export default {
-	files: ['test-new/**', '!test-new/utils'],
+	files: ['test-new/**', '!test-new/utils', '!test-new/commonjs'],
 	ignoredByWatcher: ['{coverage,docs,test-new,@types}/**'],
 	nodeArguments: [
 		'--experimental-modules'
