dbde458092546f60033f1814bfd8905624d13bf8
Konstantin Vyatkin <tino@vtkn.io>
2020-06-12 22:45:32 -0400

    remove p-timeout

--- test/utils/chai-timeout.js
+++ test/utils/chai-timeout.js
@@ -1,11 +1,8 @@
-import pTimeout from 'p-timeout';
-
 export default ({Assertion}, utils) => {
 	utils.addProperty(Assertion.prototype, 'timeout', async function () {
-		let timeouted = false;
-		await pTimeout(this._obj, 150, () => {
-			timeouted = true;
-		});
+		const timeouted = await Promise.race([this._obj.then(() => false), new Promise(resolve => {
+			setTimeout(() => resolve(true), 150);
+		})]);
 		return this.assert(
 			timeouted,
 			'expected promise to timeout but it was resolved',
