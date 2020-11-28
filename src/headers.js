10a934a12db7d4ed3023f77711e5292897374a87
Travis Warlick <twarlick@gmail.com>
2020-11-28 14:16:38 -0500

    c8 ignores for node-version-specific code and fix c8 ignore in Headers constructor

--- src/headers.js
+++ src/headers.js
@@ -7,6 +7,7 @@
 import {types} from 'node:util';
 import http from 'node:http';
 
+/* c8 ignore next 9 */
 const validateHeaderName = typeof http.validateHeaderName === 'function' ?
 	http.validateHeaderName :
 	name => {
@@ -17,6 +18,7 @@ const validateHeaderName = typeof http.validateHeaderName === 'function' ?
 		}
 	};
 
+/* c8 ignore next 9 */
 const validateHeaderValue = typeof http.validateHeaderValue === 'function' ?
 	http.validateHeaderValue :
 	(name, value) => {
@@ -141,8 +143,8 @@ export default class Headers extends URLSearchParams {
 						return Reflect.get(target, p, receiver);
 				}
 			}
-			/* c8 ignore next */
 		});
+		/* c8 ignore next */
 	}
 
 	get [Symbol.toStringTag]() {
