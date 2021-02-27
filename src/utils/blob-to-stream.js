380dba56d6b99e979f4d83187d593b430b8d8d6e
Jimmy Wärting <jimmy@warting.se>
2021-02-27 16:51:33 +0100

    Use a const

--- src/utils/blob-to-stream.js
+++ src/utils/blob-to-stream.js
@@ -1,10 +1,13 @@
 import {Readable} from 'stream';
 
+// 64 KiB (same size chrome slice theirs blob into Uint8array's)
+const POOL_SIZE = 65536
+
 /* c8 ignore start */
 async function * read(blob) {
 	let position = 0;
 	while (position !== blob.size) {
-		const chunk = blob.slice(position, Math.min(blob.size, position + 524288));
+		const chunk = blob.slice(position, Math.min(blob.size, position + POOL_SIZE));
 		// eslint-disable-next-line no-await-in-loop
 		const buffer = await chunk.arrayBuffer();
 		position += buffer.byteLength;
