3699f7ccb97dc2266ce877b483167b762c6387cc
Irakli Gozalishvili <contact@gozala.io>
2021-03-17 01:48:51 -0700

    fix: FormData handling in Response

--- src/body.js
+++ src/body.js
@@ -198,14 +198,15 @@ async function consumeBody(data) {
 
 	try {
 		for await (const chunk of body) {
-			if (data.size > 0 && accumBytes + chunk.length > data.size) {
+			const bytes = typeof chunk === "string" ? Buffer.from(chunk) : chunk
+			if (data.size > 0 && accumBytes + bytes.byteLength > data.size) {
 				const err = new FetchError(`content size at ${data.url} over limit: ${data.size}`, 'max-size');
 				body.destroy(err);
 				throw err;
 			}
 
-			accumBytes += chunk.length;
-			accum.push(chunk);
+			accumBytes += bytes.byteLength;
+			accum.push(bytes);
 		}
 	} catch (error) {
 		if (error instanceof FetchBaseError) {
@@ -218,10 +219,6 @@ async function consumeBody(data) {
 
 	if (body.readableEnded === true || body._readableState.ended === true) {
 		try {
-			if (accum.every(c => typeof c === 'string')) {
-				return Buffer.from(accum.join(''));
-			}
-
 			return Buffer.concat(accum, accumBytes);
 		} catch (error) {
 			throw new FetchError(`Could not create Buffer from response body for ${data.url}: ${error.message}`, 'system', error);
