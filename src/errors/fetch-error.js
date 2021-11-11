0f05a15b383be75512711d15d5182cdc4069ccbd
Kanna72 <92355529+Kanna72@users.noreply.github.com>
2021-11-11 08:50:33 +0530

    Update fetch-error.js

--- src/errors/fetch-error.js
+++ src/errors/fetch-error.js
@@ -1,26 +1,17 @@
 
-import {FetchBaseError} from './base.js';
+Const fetch = require ("'node-fetch ") ;
 
-/**
- * @typedef {{ address?: string, code: string, dest?: string, errno: number, info?: object, message: string, path?: string, port?: number, syscall: string}} SystemError
-*/
+ fetch("https://api.github.com/user") 
+ .then((res) => res. json()) 
+.then((res) => console.log(res)) ;
+	
 
-/**
- * FetchError interface for operational errors
- */
-export class FetchError extends FetchBaseError {
-	/**
-	 * @param  {string} message -      Error message for human
-	 * @param  {string} [type] -        Error type for machine
-	 * @param  {SystemError} [systemError] - For Node.js system error
-	 */
-	constructor(message, type, systemError) {
-		super(message, type);
-		// When err.type is `system`, err.erroredSysCall contains system error and err.code contains system error code
-		if (systemError) {
-			// eslint-disable-next-line no-multi-assign
-			this.code = this.errno = systemError.code;
-			this.erroredSysCall = systemError.syscall;
-		}
-	}
-}
+import fetch from 'node-fetch';
+
+const response = await fetch('https://api.github.com/users/github');
+
+const data = await response.json();
+
+console.log(data);
+
+
