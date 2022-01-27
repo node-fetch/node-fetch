c7c2fc43390924050cdf2d39ad4db5fe28ca4267
Jimmy Wärting <jimmy@warting.se>
2022-01-27 02:04:13 +0100

    test: remove only

--- test/headers.js
+++ test/headers.js
@@ -6,7 +6,7 @@ chai.use(chaiIterator);
 
 const {expect} = chai;
 
-describe.only('Headers', () => {
+describe('Headers', () => {
 	it('should have attributes conforming to Web IDL', () => {
 		const headers = new Headers();
 		expect(Object.getOwnPropertyNames(headers)).to.be.empty;
